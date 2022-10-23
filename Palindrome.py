6489cd93098fec62408508d82cf0fae97f49753a
Shavukarusasikumar <98202742+Shavukarusasikumar@users.noreply.github.com>
2022-10-23 11:55:01 +0530

    def rev(num):     r=0     while num:         d=num%10         num=num//10         r=r*10+d     return r
    num=int(input())
    a=rev(num)
    if(a==num):
        print("True")
    else:
        print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,14 @@
-n=int(input())
-t=n
-r=0
-while n:
-    d=n%10
-    r=r*10+d
-    n=n//10
-if r==t:
+def rev(num):
+    r=0
+    while num:
+        d=num%10
+        num=num//10
+        r=r*10+d
+    return r
+
+num=int(input())
+a=rev(num)
+if(a==num):
     print("True")
 else:
     print("False")
