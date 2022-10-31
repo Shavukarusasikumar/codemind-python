3f6e7e37302f77314926bf43ec3cacf6543433be
Shavukarusasikumar <98202742+Shavukarusasikumar@users.noreply.github.com>
2022-10-31 14:49:13 +0530

    def p(num):     pf=0     for i in range(1,num):         if(num%i==0):             pf+=i     return pf a=int(input()) b=int(input()) if(p(a)==b and p(b)==a):     print('Amicable') else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,12 @@
+def p(num):
+    pf=0
+    for i in range(1,num):
+        if(num%i==0):
+            pf+=i
+    return pf
 a=int(input())
 b=int(input())
-pa=0
-for i in range(1,a):
-    if a%i==0:
-        pa+=i
-pb=0
-for i in range(1,b):
-    if b%i==0:
-        pb+=i
-if(pa==b and pb==a):
-    print("Amicable")
+if(p(a)==b and p(b)==a):
+    print('Amicable')
 else:
-    print('Not Amicable')
+    print("Not Amicable")
